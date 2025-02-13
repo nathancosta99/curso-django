63ecc7950d3189c22cf9bbb07430000e484c42a8
nathancosta99 <nathan@datalabpesquisas.com>
2025-02-13 13:46:46 -0300

    Pages

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,15 @@
-from django.shortcuts import render,get_list_or_404,get_object_or_404
-from utils.recipes.factory import make_recipe
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from recipes.models import Recipe
 
 
-
 def home(request):
-    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
-    return render(request, 'recipes/pages/home.html', context={
+    recipes = get_list_or_404(Recipe.objects.filter(
+        is_published=True
+        ).order_by('-id'))
+
+    return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
+        'title': f'{recipes[0].category.name}  - Category | '
     })
 
 
@@ -22,7 +24,8 @@ def category(request, category_id):
 
 
 def recipe(request, id):
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True)
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True,
     })
